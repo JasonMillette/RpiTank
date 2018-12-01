5f3edac1dec215719afa4d5386149decd2977d60
Jason Millette <jason.millette@maine.edu>
2018-12-01 02:18:33 +0000

    Launches tank.c on startup

--- tank.c
+++ tank.c
@@ -10,27 +10,22 @@
 #include <sys/time.h>
 #include <unistd.h>
 
-#include "tank.h"
-#include "motorcontrol.h"
-
 int main() {
 
-    system("./initDisplay.py");         //Initialize the SPI displays
-    system("./IP.py");                  //Displays the IP address for webpage
+    system("/home/pi/Documents/ECE471/project/RpiTank/./initDisplay.py");         //Initialize the SPI displays
+    system("/home/pi/Documents/ECE471/project/RpiTank/./IP.py");                  //Displays the IP address for webpage
 
     int weatherPID = fork();
 
     if (weatherPID == 0) {
         while(1) {
-            system("./weather.py");
-            printf("Weather\n");
+            system("/home/pi/Documents/ECE471/project/RpiTank/./weather.py");
             sleep(120);
         }
     }
 
     else {
-        printf("meme\n");
-        system("./meme.py");
+        system("/home/pi/Documents/ECE471/project/RpiTank/./meme.py");
     }
 
     return 0;
