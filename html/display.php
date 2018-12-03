f2a42d800425f0e34b30245e707cc684202b5ae7
Jason Millette <jason.millette@maine.edu>
2018-12-03 02:43:14 +0000

    changed display.php

---
+++ html/display.php
@@ -0,0 +1,27 @@
+﻿<?php
+//Jason Millette
+//gets data from html webpage, then calls call display function
+//to display on SPI device
+
+//Getting and using values
+if (isset ( $_GET["status"] )) {
+	$status = strip_tags ($_GET["status"]);
+	
+	// calling function to display status
+	//exec("/usr/bin/python2.7 /var/www/html/infoDisplay.py ".$status, $error, $return);
+	//displaying status for debug
+        
+	exec("./infoDisplay.py ".$status, $error, $return);
+
+        //exec("ls -".$status, $error, $return);
+
+        print_r(array_values($error));
+        echo($return."\n");
+
+
+        
+
+
+        
+}
+?>
---
+++ infoDisplay.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#Jason Millette
+#11/23/18
+
+#uses information from the website to display
+#then create an 128x128 image for display on an SPI device
+
+import requests
+import Image
+import ImageDraw
+import sys
+import ST7735 as TFT
+import Adafruit_GPIO.SPI as SPI
+
+#Getting weather data
+
+
+#Creating weather image for SPI display
+image = Image.new('RGB', (128,128), (0, 0, 255))    #RGB, size, color
+draw = ImageDraw.Draw(image)                        #creates image
+draw.text((10,9), 'Direction: ' + sys.argv[1], fill=(0, 0, 0))#(x,y), text, (R, G, B)
+#draw.text((35, 18), 'sys.argv[2]', fill=(0, 0, 0))
+
+#displaying images to spi devices
+DC = 24
+RST = 25
+
+#Displaying website data
+display2 = TFT.ST7735(DC, rst=RST, spi=SPI.SpiDev(0, 1, max_speed_hz=8000000)) # display initalized in initDisplay.py
+display2.display(image) #displays image
+
