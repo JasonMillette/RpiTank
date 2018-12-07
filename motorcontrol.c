b09fbe0cd2557cca4ba77f6a7fbc2e61655c9aa4
Spencer Goulette <32938977+SpencerGoulette@users.noreply.github.com>
2018-12-06 22:57:37 -0500

    Added comments, headlights, and backup lights

--- motorcontrol.c
+++ motorcontrol.c
@@ -9,9 +9,14 @@
 
 int main(int argc, char **argv)
 {
+	// Initializes GPIO pins
 	init_motor();
+
+	// Converts input to doubles
 	double x = atof(argv[1]);
 	double y = atof(argv[2]);
+
+	// Need to constantly run motors
 	while(1)
 	{
 		run_motor(x,y);
@@ -20,68 +25,104 @@ int main(int argc, char **argv)
 
 int init_motor(void)
 {
+	// Enables all GPIO pins needed for motors
 	gpio_enable(dir1);
 	gpio_enable(dir2);
 	gpio_enable(step1);
 	gpio_enable(step2);
+	gpio_enable(ledfor);
+	gpio_enable(ledback);
 	gpio_set_output(dir1);
 	gpio_set_output(dir2);
 	gpio_set_output(step1);
 	gpio_set_output(step2);
+	gpio_set_output(ledfor);
+	gpio_set_output(ledback);
 
 	return 0;
 }
 
 int run_motor(double dx, double dy)
 {
+	// Sets initial delay for motor speed
 	double udelay = 0;
 	
+	// Has issue with same values so quick fix
 	if(dx == dy || dx == -dy)
 	{
 		return 0;
 	}
 
+	// 'normalizes' coordinates
 	dx = dx / maxValue;
 	dy = dy / maxValue;
 
+	// Headlights on 
+	if(dy >= 0)
+	{
+		gpio_set_output_high(ledfor);
+		gpio_set_output_low(ledback);
+	}
+	// Backup lights
+	else
+	{
+		gpio_set_output_high(ledback);
+		gpio_set_output_low(ledfor);
+	}
+
+	// Forks to run both motors
 	int PID = fork();
 
+	// Left Motor
 	if(PID == 0)
 	{
 		if(dy >= 0)
 		{
+			// Left Motor Quadrant 1 Math
 			if(dx >= 0)
 			{
 				udelay = (sqrt(dx * dx + dy * dy)); 
 			}
+			// Left Motor Quadrant 2 Math
 			else
 			{
 				udelay = ((dy * dy - dx * dx) / sqrt(dy * dy + dx * dx));
 			}
 		}
+
 		if(dy < 0)
 		{
+			// Left Motor Quadrant 3 Math
 			if(dx < 0)
 			{
 				udelay = -(sqrt(dx * dx + dy * dy));
 			}
+			// Left Motor Quadrant 4 Math
 			else
 			{
 				udelay = -((dy * dy - dx * dx) / sqrt(dy * dy + dx * dx));
 			}	
 		}
+		
+		// Makes delay positive and makes motor go backwards
 		if(udelay < 0)
 		{
 			gpio_set_output_high(dir1);
 			udelay *= -1;
 		}
+
+		// Makes motor go forward
 		else{
 			gpio_set_output_low(dir1);
 		}
+
+		// Caps delay
 		if(udelay > 1)
 		{
 			udelay = 1;
 		}
+
+		// Delay Math for motor speed
 		if(udelay != 0)
 		{
 			udelay = maxDelay - coDelay * udelay;
@@ -89,17 +130,21 @@ int run_motor(double dx, double dy)
 			gpio_set_output_low(step1);
 			usleep(udelay);
 		}
+		// Exits fork
 		exit(0);
 	}
 
+	// Right Motor
 	else
 	{
 		if(dy >= 0)
-		{
+		{	
+			// Right Motor Quadrant 1 Math
 			if(dx >= 0)
 			{
 				udelay = ((dy * dy - dx * dx) / sqrt(dy * dy + dx * dx));
 			}
+			// Right Motor Quadrant 2 Math
 			else
 			{
 				udelay = (sqrt(dx * dx + dy * dy));
@@ -107,27 +152,37 @@ int run_motor(double dx, double dy)
 		}
 		if(dy < 0)
 		{
+			// Right Motor Quadrant 3 Math
 			if(dx < 0)
 			{
 				udelay = -((dy * dy - dx * dx) / sqrt(dy * dy + dx * dx));
 			}
+			// Right Motor Quadrant 4 Math
 			else
 			{
 				udelay = -(sqrt(dx * dx + dy * dy));
 			}
 		}
+		
+		// Makes delay positive and motor go backwards 
 		if(udelay < 0)
 		{
 			gpio_set_output_low(dir2);
 			udelay *= -1;
 		}
+
+		// Makes motor go forwards
 		else{
 			gpio_set_output_high(dir2);
 		}
+
+		// Caps delay
 		if(udelay > 1)
 		{
 			udelay = 1;
 		}
+
+		// Motor delay math for motor speed
 		if(udelay != 0)
 		{
 			udelay = maxDelay - coDelay * udelay;
--- motorcontrol.h
+++ motorcontrol.h
@@ -2,6 +2,8 @@
 #define step2 19
 #define dir1 13
 #define dir2 26
+#define ledfor 16
+#define ledback 20
 #define maxValue 750.0
 #define maxDelay 15000
 #define coDelay 10000
